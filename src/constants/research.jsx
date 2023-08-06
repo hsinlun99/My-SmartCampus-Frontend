bf187753c816e30db78aa6ca00517c8fae3c4901
hsinlun99 <hsinlun.chiu@gmail.com>
2023-08-06 19:54:20 +0800

    fix detailPart conflic

---
+++ src/constants/research.jsx
@@ -0,0 +1,16 @@
+export default [
+  [
+    {
+      statusName: '存在',
+      statusColor: '#dce775',
+      statusIcon: SlovedIcon,
+      statusOnIcon: OnSlovedIcon
+    },
+    {
+      statusName: '資訊有誤',
+      statusColor: '#FFB59F',
+      statusIcon: UnslovedIcon,
+      statusOnIcon: OnUnslovedIcon
+    }
+  ],
+]
